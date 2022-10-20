33ededf7acdac8ce4c86a60335df42fc87e007f5
gtato <genc.tato@canonical.com>
2022-10-20 14:32:38 +0200

    Expect the encryption key to be a base64 string

--- tests/encryption/test_crypt.py
+++ tests/encryption/test_crypt.py
@@ -1,8 +1,11 @@
+import base64
+
 from temporallib.encryption import decrypt, encrypt
 
 
 def test_encode_decode():
-    key = b"[ 16 bytes key ]"
+    b64_key = "HLCeMJLLiyLrUOukdThNgRfyraIXZk918rtp5VX/uwI="
+    key = base64.b64decode(b64_key)
     secret_msg = b"this is top secret"
     enc_msg = encrypt(secret_msg, key)
     dec_msg = decrypt(enc_msg, key)
