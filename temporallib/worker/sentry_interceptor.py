45b98f8e7701bb9939dcfc6bee60b8eb3639108c
Ali Kelkawi <ali.kelkawi@canonical.com>
2023-09-08 14:59:52 +0300

    refactor redact_params method

--- temporallib/worker/sentry_interceptor.py
+++ temporallib/worker/sentry_interceptor.py
@@ -97,13 +97,17 @@ class SentryInterceptor(Interceptor):
 
 def redact_params(event, hint):
     # Redact parameters from captured events
-    if "exception" in event:
-        if "values" in event["exception"]:
-            for exc in event["exception"]["values"]:
-                if "stacktrace" in exc:
-                    for frame in exc["stacktrace"]["frames"]:
-                        # Filter out specific parameter keys
-                        if "vars" in frame:
-                            frame["vars"] = {key: "REDACTED" for key in frame["vars"]}
+    if "exception" not in event:
+        return event
+    if "values" not in event["exception"]:
+        return event
+
+    for exc in event["exception"]["values"]:
+        if "stacktrace" not in exc:
+            continue
+        for frame in exc["stacktrace"]["frames"]:
+            # Filter out specific parameter keys
+            if "vars" in frame:
+                frame["vars"] = {key: "REDACTED" for key in frame["vars"]}
 
     return event
